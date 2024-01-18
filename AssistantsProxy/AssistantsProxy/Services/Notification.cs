cd3785768a62ce02de5973d9c9a002ab90582abc
John Taylor <johtaylo@microsoft.com>
2024-01-17 23:18:24 -0800

    signalr notifications instead of polling
    small refactoring

---
+++ AssistantsProxy/AssistantsProxy/Services/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AssistantsProxy.Services
+{
+    public class Notification
+    {
+        [JsonPropertyName("thread_id")]
+        public required string ThreadId { get; init; }
+
+        [JsonPropertyName("run_id")]
+        public required string RunId { get; init; }
+
+        [JsonPropertyName("status")]
+        public required string Status { get; init; }
+    }
+}
